4226929000cccc9b699a078bd44eb65a82df35dd
Divyagoud122 <Thatikondadivyagoud122@gmail.com>
2024-07-19 11:52:21 +0530

    Create FirstProgram.java

---
+++ FirstProgram.java
@@ -0,0 +1,20 @@
+package Practical;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// Writing about myself
+		System.out.println("My name is T.Divya.");
+		System.out.println("I am from Hyderabad.");
+		System.out.println("I am studying MCA 1st year at Avanthi PG college. ");
+		System.out.println("I have completed my B.sc from Ideal Degree college.");
+		System.out.println("I have a small family");
+		System.out.println("In my family my mother,sister,and me");
+		System.out.println("Currently my sister is studying B.tech in Weseley Engineering College");
+		System.out.println("My mother is a Homemaker");
+		System.out.println("My Hobbies are Listening music and watching movies ");
+		
+
+	}
+
+}
