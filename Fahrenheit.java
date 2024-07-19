1bb70617654f29fd9f6aebc5884087261df15eb1
Divyagoud122 <Thatikondadivyagoud122@gmail.com>
2024-07-19 10:55:32 +0530

    Create Fahrenheit.java

---
+++ Fahrenheit.java
@@ -0,0 +1,11 @@
+package day2;
+public class Fahreanheit{
+  public static void main(STring args[]){
+    double fahrenheit=100;
+    double convert=fahrenheit-32;
+    double formula=5.0/9.0;
+    double celsius=(convert * formula);
+    System.out.println("fahrenheit");
+    System.out.println("celsius");
+  }
+}
