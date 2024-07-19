4e6dd32e4e14f74109161f881c8add3291165b35
Divyagoud122 <Thatikondadivyagoud122@gmail.com>
2024-07-19 10:49:51 +0530

    Create SimpleInterest.java

---
+++ SimpleInterest.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class Simple_interest {
+
+	public static void main(String[] args) {
+		// to calculate simple interest
+		double principal = 9000; // Principal amount
+	    float rate = 3.5f; // Interest rate in percentage
+	    double time = 2; // Time in years
+
+	    double interest = (principal * rate * time) / 100;
+
+	    System.out.println("Simple Interest: " + interest);
+		
+		
+	}
+
+}
